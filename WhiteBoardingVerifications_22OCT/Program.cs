1d4d8571c366e8cadf6dac1a840a03b22da932a9
Joseph Callahan <Joseph.callahan1@outlook.com>
2018-10-22 18:10:09 -0400

    Add project files.

---
+++ WhiteBoardingVerifications_22OCT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteBoardingVerifications_22OCT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
